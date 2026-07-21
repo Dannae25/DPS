0ecc737adec5ab686b81707f2ef0fbd226a75781
Dannae25 <danae.g2507@gmail.com>
2026-07-20 22:52:29 -0600

    Agregar: El modelo de datos que contiene los productos

---
+++ src/data/carros.ts
@@ -0,0 +1,196 @@
+import { Productos } from "@/types/productos" 
+
+export const carros: Productos[] = [
+  {
+    id: 1,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 2,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 3,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 4,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 5,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 6,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 7,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 8,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 9,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 10,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 11,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 12,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 13,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 14,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 15,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 17,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 18,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 19,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+  {
+    id: 20,
+    titulo: "Carro de nueva generación",
+    precio: 1000,
+    urlImage: "",
+    categoria: "",
+    descripcion: "",
+    cantidad: 10
+
+  },
+
+];
+
---
+++ src/types/productos.ts
@@ -0,0 +1,9 @@
+export interface Productos {
+  id: number;
+  titulo: string;
+  precio: number;
+  urlImage: string;
+  categoria: string;
+  descripcion: string;
+  cantidad: number;
+}
