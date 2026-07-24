c57d8b25e886b0a9b78d8da43e4db9bc517b35c7
Dannae25 <danae.g2507@gmail.com>
2026-07-24 17:41:26 -0600

    Optimizar: Estilos en el modo oscuro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {Toaster} from "sonner";
+import { Toaster } from "sonner";
 import { ProveedorCarrito } from "@/context/cartContex";
 import { ProveedorAuth } from "@/context/usuarioContex";
 import Navbar from "@/componentes/navbar";
-import Footer from "@/componentes/footer"
+import Footer from "@/componentes/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,6 +20,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "LaptopFriend",
   description: "E-commerce",
+  colorScheme: "light",
 };
 
 export default function RootLayout({
@@ -30,18 +31,18 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-screen flex flex-col" >
+      <body className="min-h-screen flex flex-col bg-stone-50 text-stone-900 transition-colors duration-200">
         <ProveedorAuth>
-        <ProveedorCarrito>
-        <Navbar />
-        <main className="flex-1">{children}</main>
-        <Toaster position="top-center" richColors />
-        </ProveedorCarrito>
+          <ProveedorCarrito>
+            <Navbar />
+            <main className="flex-1">{children}</main>
+            <Toaster position="top-center" richColors />
+          </ProveedorCarrito>
         </ProveedorAuth>
         <Footer />
-
       </body>
     </html>
   );
